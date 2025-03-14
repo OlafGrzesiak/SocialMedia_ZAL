2411bf332b1866cc64aab0cec753afc4e980305c
OlafGrzesiak <olaf.grzesiak@microsoft.wsei.edu.pl>
2025-03-14 16:00:35 +0100

    App

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./Context/AuthContext";
+import LoginPage from "./Pages/LoginPage";
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/feed" element={<h1>Feed ze zdjęciami (do zrobienia)</h1>} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+};
+
+export default App;
