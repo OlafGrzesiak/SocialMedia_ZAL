ed35ace5d48a2ee82b7956d59ab76354b93a5488
OlafGrzesiak <olaf.grzesiak@microsoft.wsei.edu.pl>
2025-03-14 15:57:18 +0100

    Login Page and Login Form

---
+++ Pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import LoginForm from "../Components/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
